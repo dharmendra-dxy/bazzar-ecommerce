0455df744e3b52749fd3bc1074c9964567fcba24
Dharmendra <yaduvanshi.dhanno@gmail.com>
2025-04-21 01:48:01 +0530

    (server): initialize server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,41 @@
 import { PrismaClient } from "@prisma/client";
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from "cookie-parser";
 
+import authRoutes from './routes/auth.routes';
 
+dotenv.config();
+const app = express();
+const PORT=process.env.PORT ?? 3001;
 
 // prisma client:
 export const prisma = new PrismaClient();
+
+// configure cors options:
+const corsOption = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}
+
+app.use(cors(corsOption));
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use('/api/auth', authRoutes);
+app.get('/', (req,res) =>{ 
+    res.json({ message:"Hello from bazaar"});
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running at: http://localhost:${PORT}`);
+});
+
+process.on('SIGINT', async()=>{
+    await prisma.$disconnect();
+    process.exit();
+})
