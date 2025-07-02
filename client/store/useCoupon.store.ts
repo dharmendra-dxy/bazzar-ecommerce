774f1701ddcc701191aefdaa7561172fa2a6a436
Dharmendra <yaduvanshi.dhanno@gmail.com>
2025-07-02 23:59:12 +0530

    (client): useCoupon store created

---
+++ client/store/useCoupon.store.ts
@@ -0,0 +1,76 @@
+import { API_ROUTES } from "@/utils/api";
+import axios from "axios";
+import { create } from "zustand";
+
+export interface Coupon {
+    id: string,
+    code: string,
+    discountPercent: number,
+    startDate: string,
+    endDate: string,
+    usageLimit: number,
+    usageCount: number,
+}
+
+interface CouponStore {
+    coupons: Coupon[],
+    isLoading: boolean,
+    error: string | null,
+    fetchCoupon: () => Promise<void>,
+    createCoupon : (coupon: Omit<Coupon, 'id' | 'usageCount'>) => Promise<Coupon | null>,
+    deleteCoupon : (id:string) => Promise<Coupon | null>,
+}
+
+export const useCouponStore = create<CouponStore>((set,get)=>({
+    coupons: [],
+    isLoading: false,
+    error: null,
+    fetchCoupon: async ()=>{
+        set({isLoading: true, error:null});
+        try{
+            const response = await axios.get(`${API_ROUTES.COUPONS}/all`,{
+                withCredentials: true,
+            });
+
+            set({coupons: response.data?.coupon, isLoading: false});
+        }
+        catch(err){
+            set({isLoading: false, error:'Failed to fetch coupons'});   
+        }
+    },
+    
+    createCoupon: async(coupon) => {
+        set({isLoading: true, error:null});
+        try{
+            const response = await axios.post(`${API_ROUTES.COUPONS}/new`, coupon, {
+                withCredentials: true,
+            });
+
+            set({isLoading: false});
+            return response.data?.coupon;
+        }
+        catch(err){
+            set({isLoading: false, error:'Failed to create coupons'}); 
+            return null;  
+        }
+
+    },
+
+    deleteCoupon: async(id: string) => {
+        set({isLoading: true, error:null});
+        try{
+            const response = await axios.delete(`${API_ROUTES.COUPONS}/${id}`, {
+                withCredentials: true,
+            });
+
+            set({isLoading: false});
+            return response.data?.coupon;
+        }
+        catch(err){
+            set({isLoading: false, error:'Failed to create coupons'}); 
+            return null;  
+        }
+
+    }
+})
+)
--- client/utils/api.ts
+++ client/utils/api.ts
@@ -3,4 +3,5 @@ export const API_BASE_URL= 'http://localhost:3003';
 export const API_ROUTES= {
     AUTH: `${API_BASE_URL}/api/auth`,
     PRODCUTS: `${API_BASE_URL}/api/products`,
+    COUPONS: `${API_BASE_URL}/api/coupons`,
 }
